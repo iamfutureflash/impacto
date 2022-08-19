d9b846045818e0294ff0fe1f8217c680afbc6ddb
iamfutureflash <xenode2000@gmail.com>
2022-08-20 04:45:52 +0530

    started intent StatsFragment.this.getActivity() --> QuestionActivity.class intent work if limit exceded

--- app/src/main/java/com/afdroid/timetracker/QuestionActivity.java
+++ app/src/main/java/com/afdroid/timetracker/QuestionActivity.java
@@ -1,7 +1,13 @@
 package com.afdroid.timetracker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.afdroid.timetracker.screens.MainActivity;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -9,5 +15,16 @@ public class QuestionActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+    }
+
+    public void move_to_main(View view) {
+        try{
+            Intent intent = new Intent(QuestionActivity.this, MainActivity.class);
+            Toast.makeText(this, "started intent QuestionActivity.this -> SettingsActivity.class", Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+        }catch (Exception e){
+            Context context;
+            Toast.makeText(this, " " + e, Toast.LENGTH_SHORT).show();
+        }
     }
 }
